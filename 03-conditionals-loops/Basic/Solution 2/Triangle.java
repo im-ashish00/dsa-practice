377d78fd90bef012e91fe77c0213278bf4e78be1
im-ashish00 <imashishsharma00@gmail.com>
2022-07-04 15:30:53 +0530

    03 Basic Solution 2

---
+++ 03-conditionals-loops/Basic/Solution 2/Triangle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the base and a height of the triangle : ");
+        int base = input.nextInt();
+        int height = input.nextInt();
+        System.out.print("Area of the Triangle is " + 0.5 * base * height);
+    }
+}
