ceaaf85d73c9b73c83b46a68c29254e56d99af14
im-ashish00 <imashishsharma00@gmail.com>
2022-07-04 18:07:53 +0530

    03 Basic Solution 22

---
+++ 03-conditionals-loops/Basic/Solution 22/Factors.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Factors {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number = input.nextInt();
+        System.out.println("Factors of the number are: ");
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
